896cc93e0acc7f8fa34bf8fbc6aa250f894dd33c
Antonio Carmona López <antocarmona@gmail.com>
2018-03-06 11:58:06 +0100

    anyadir comentaris List2.java

--- Lista2.java
+++ Lista2.java
@@ -25,9 +25,12 @@ public class Lista2 {
 
     public void insertarAlPrincipio(Object dato) {
         Nodo n = new Nodo(dato);
+//si la lista esta vacia el primerNodo y ultimoNodo se encuentran null
+//y se le asigna el nuevo dato
         if (estaVacia()) {
             primerNodo = n;
             ultimoNodo=n;
+//si no esta vacia hacemos que el nuevo dato apunte al siguiente nodo y asimismo hacemos que el nodo anteror al nuevo nodo apunte al nuevo nodo
         } else {
             n.setSig(primerNodo);
             primerNodo = n;
@@ -37,22 +40,27 @@ public class Lista2 {
 
     public void insertarAlFinal(Object dato) {
         Nodo n = new Nodo(dato);
+//si esta vacia idem insertarInicio
         if (estaVacia()) {
             ultimoNodo = n;
             primerNodo=n;
         } else {
+//si existen datos hacemos que el penultimo nodo ahora apunte al nuevo nnodo y que el nodo insertado apunte al penúltimo nodo
             Nodo aux = primerNodo;
             while (aux.getSig() != null) {
                 aux = aux.getSig();
             }
             primerNodo.setSig(n);
-            ultimoNodo.setAnt(n);
+            ultimoNodo.setAnt(primerNodo);
         }
     }
 
     public boolean eliminarDelPrincipio() {
+//si esta vacia no hacemos nada
         if (estaVacia()) {
             return false;
+//el el caso de que si existan datos en la lista hacemos que el primerNodo
+//apunte por un extremo del nodo al siguiente nodo y por el final de la lista hacemos que apunte a null
         } else {
             primerNodo = primerNodo.getSig();
             primerNodo.setAnt(null);
@@ -63,11 +71,15 @@ public class Lista2 {
 
  public boolean eliminarDelFinal() {
         if (estaVacia()) {
+//si esta vacia no hacemos nada
             return false;
         } else {
+//si el útimo nodo no apunta a nada(null), simplemente se le asigna el
+// valor null a primerNodo
             Nodo aux = primerNodo;
             if (aux.getSig() == null) {
                 primerNodo = null;
+//si no el el último hacemos que el último nodo apunte a null
             } else {
                 ultimoNodo.setSig(null);
             }
